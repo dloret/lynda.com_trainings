ef7c3b07d8c20fdda7e572d1ee5ed018eaf4ee9a
Donatien LORET <donatienloret@gmail.com>
2019-06-08 13:15:36 +0200

    Learning Redux: object reducer - skiDay

--- learning_redux/src/store/reducers.js
+++ learning_redux/src/store/reducers.js
@@ -2,9 +2,18 @@ import C from '../constants';
 
 export const goal = (state = 10, action) => {
   switch (action.type) {
-    case C.SET_GOAL: return parseInt(action.payload, 10);
-    default: return state;
+    case C.SET_GOAL:
+      return parseInt(action.payload, 10);
+    default:
+      return state;
   }
 };
 
-export const next = null;
+export const skiDay = (state = {}, action) => {
+  switch (action.type) {
+    case C.ADD_DAY:
+      return { ...state, ...action.payload };
+    default:
+      return state;
+  }
+};
