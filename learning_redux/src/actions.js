ea67eb3d27ababb252fb973f77f2f723fef1632b
Donatien LORET <donatien.loret@sap.com>
2019-06-21 15:43:14 +0200

    Learning Redux: finished course

--- learning_redux/src/actions.js
+++ learning_redux/src/actions.js
@@ -1,62 +1,79 @@
-import fetch from 'isomorphic-fetch';
-import C from './constants';
+import C from './constants'
+import fetch from 'isomorphic-fetch'
 
-export function addDay(resort, date, powder = false, backcountry = false) {
-  return {
-    type: C.ADD_DAY,
-    payload: {
-      resort,
-      date,
-      powder,
-      backcountry,
-    },
-  };
+export function addDay(resort, date, powder=false, backcountry=false) {
+
+    return {
+        type: C.ADD_DAY,
+        payload: {resort,date,powder,backcountry}
+    }
+
 }
 
-export const removeDay = date => ({
-  type: C.REMOVE_DAY,
-  payload: date,
-});
+export const removeDay = function(date) {
 
-export const setGoal = goal => ({
-  type: C.SET_GOAL,
-  payload: goal,
-});
+    return {
+        type: C.REMOVE_DAY,
+        payload: date
+    }
 
-export const addError = message => ({
-  type: C.ADD_ERROR,
-  payload: message,
-});
+}
 
-export const clearError = index => ({
-  type: C.CLEAR_ERROR,
-  payload: index,
-});
+export const setGoal = (goal) => 
+    ({
+        type: C.SET_GOAL,
+        payload: goal
+    })
 
-export const changeSuggestions = suggestions => ({
-  type: C.CHANGE_SUGGESTIONS,
-  payload: suggestions,
-});
+export const addError = (message) => 
+   ({
+      type: C.ADD_ERROR,
+      payload: message
+   })
 
-export const clearSuggestions = () => ({
-  type: C.CLEAR_SUGGESTIONS,
-});
+export const clearError = index => 
+    ({
+        type: C.CLEAR_ERROR,
+        payload: index
+    })   
 
-export const suggestResortNames = value => (dispatch) => {
-  dispatch({
-    type: C.FETCH_RESORT_NAMES,
-  });
+export const changeSuggestions = suggestions => 
+  ({
+    type: C.CHANGE_SUGGESTIONS,
+    payload: suggestions
+  })
 
-  fetch(`http://localhost:3333/resorts/${value}`)
-    .then(resp => resp.json())
-    .then(json => dispatch({
-      type: C.CHANGE_SUGGESTIONS,
-      payload: json,
-    }))
-    .catch((error) => {
-      dispatch(addError(error));
-      dispatch({
-        type: C.CANCEL_FETCHING,
-      });
-    });
-};
+export const clearSuggestions = () => 
+    ({
+        type: C.CLEAR_SUGGESTIONS
+    })
+
+export const suggestResortNames = value => dispatch => {
+
+    dispatch({
+        type: C.FETCH_RESORT_NAMES
+    })
+
+    fetch('http://localhost:3333/resorts/' + value)
+        .then(response => response.json())
+        .then(suggestions => {
+
+            dispatch({
+                type: C.CHANGE_SUGGESTIONS,
+                payload: suggestions
+            })
+
+        })
+        .catch(error => {
+
+            dispatch(
+                addError(error.message)
+            )
+
+            dispatch({
+                type: C.CANCEL_FETCHING
+            })
+
+        })
+
+}
